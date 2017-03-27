9a0e0310843893fafd2ba7f46d8724e2c86f2046
amessner <alexander.messner.am@gmail.com>
2017-03-27 11:21:42 +0200

    LINK

--- src/alexander/Test1703.java
+++ src/alexander/Test1703.java
@@ -1,7 +1,9 @@
 package alexander;
 
 public class Test1703 {
-
+ /*
+  * git@github.com:AlexMessner/Test1703.git
+  */
 	public static void main(String[] args) {
 		System.out.println("Hallo alexander");
 		System.out.println("Hallo alexander");
